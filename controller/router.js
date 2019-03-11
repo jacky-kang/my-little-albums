b4093e16e301f7f29baa91644f7f25c9748eec81
小康 <baidu@google.com>
2019-03-11 17:36:24 +0800

    添加下载功能,列表页显示文件名,多文件上传

--- controller/router.js
+++ controller/router.js
@@ -1,84 +1,99 @@
-const file = require('../models');
-const formidable = require('formidable');
-const path = require('path');
-const fs = require('fs');
-const fsExtra = require('fs-extra');
-module.exports.showIndex = function(req,res){
-    file.getAllAlbums((allAlbums)=>{
-        res.render('index',{
-            'albums':allAlbums
-        });
-    });
-};
-module.exports.showAlbum = function(req,res){
-    var albumName = req.params.albumName;
-    file.getAllPhotos(albumName,(photos)=>{
-        res.render('album',{
-            'albumName':albumName,
-            'photos': photos
-        });
-    });
-};
-module.exports.dopost = function(req,res){
-    var form = new formidable.IncomingForm();
-    var targetFile = path.join(__dirname,'../tempup');
-    var final = path.join(__dirname,'../upload');
-    form.uploadDir = targetFile;
-    form.parse(req,function(err,fields,files){
-        if(err){
-            throw err;
-        };
-        var oldpath = files.picture.path;
-        var newpath = path.join(final,fields.flodername,files.picture.name);
-        fs.rename(oldpath,newpath,(err)=>{
-            if(err) throw err;
-            res.redirect('/'+fields.flodername);
-        });
-    });
-};
-module.exports.newfloder = function(req,res){
-    var form = new formidable.IncomingForm();
-    form.parse(req,function(err,fields,file){
+const file = require('../models')
+const formidable = require('formidable')
+const path = require('path')
+const fs = require('fs')
+const fsExtra = require('fs-extra')
+const dayJs = require('dayjs')
+
+module.exports.showIndex = function (req, res) {
+  file.getAllAlbums((allAlbums) => {
+    res.render('index', {
+      'albums': allAlbums
+    })
+  })
+}
+module.exports.showAlbum = function (req, res) {
+  let albumName = req.params.albumName
+  file.getAllPhotos(albumName, (photos) => {
+    res.render('album', {
+      'albumName': albumName,
+      'photos': photos
+    })
+  })
+}
+module.exports.doPost = function (req, res) {
+  let form = new formidable.IncomingForm()
+  form.multiples = true
+
+  let targetFile = path.join(__dirname, '../temp-up')
+  let final = path.join(__dirname, '../upload')
+  form.uploadDir = targetFile
+
+  form.parse(req,  (err, fields, files) => {
+    if (err) {
+      throw err
+    }
+
+    files['picture'].forEach((file, i) => {
+      let oldPath = file.path
+      let ext = path.extname(file.name)
+      let name = file.name.replace(ext, dayJs().format('_YYYY-MM-DD_HH:mm:ss') + ext)
+      let newPath = path.join(final, fields['folderName'], name)
+
+
+      fs.rename(oldPath, newPath, err=>{
         if(err) throw err;
-        var target = path.join(__dirname,'../upload',fields.flodername);
-        fs.mkdir(target,(err)=>{
-            if(err) throw err;
-            res.send('1');
-        });
-    });
-};
-module.exports.rename = function(req,res){
-    var form = new formidable.IncomingForm();
-    form.parse(req,function(err,fields,file){
-        if(err) throw err;
-        var oldpath = path.join(__dirname,'../upload',fields.oldname);
-        var newpath = path.join(__dirname,'../upload',fields.newname);
-        fs.rename(oldpath,newpath,(err)=>{
-            if(err) throw err;
-            res.send('1');
-        });
-    });
-};
-module.exports.deletefloder = function(req,res){
-    var form = new formidable.IncomingForm();
-    form.parse(req,function(err,fields,file){
-        if(err) throw err;
-        var targetfloder = path.join(__dirname,'../upload',fields.flodername);
-        fsExtra.remove(targetfloder,(err)=>{
-            if(err) throw err;
-            res.send('1');
-        });
-    });
-};
-module.exports.deleteimg = function(req,res){
-    var form = new formidable.IncomingForm();
-    form.parse(req,function(err,fields,file){
-        if(err) throw err;
-        var img = path.basename(fields.imgsrc);
-        var target = path.join(__dirname,'../upload',fields.flodername,img);
-        fsExtra.remove(target,(err)=>{
-            if(err) throw err;
-            res.send('1');
-        });
-    });
-};
+        else if (files['picture'].length === i + 1) {
+          res.redirect('/' + fields['folderName'])
+        }
+      })
+
+    })
+  })
+}
+module.exports.newFolder = function (req, res) {
+  let form = new formidable.IncomingForm()
+  form.parse(req, function (err, fields, file) {
+    if (err) throw err
+    let target = path.join(__dirname, '../upload', fields['flodername'])
+    fs.mkdir(target, (err) => {
+      if (err) throw err
+      res.send('1')
+    })
+  })
+}
+module.exports.rename = function (req, res) {
+  let form = new formidable.IncomingForm()
+  form.parse(req, function (err, fields, file) {
+    if (err) throw err
+    let oldPath = path.join(__dirname, '../upload', fields.oldname)
+    let newPath = path.join(__dirname, '../upload', fields.newname)
+    fs.rename(oldPath, newPath, (err) => {
+      if (err) throw err
+      res.send('1')
+    })
+  })
+}
+module.exports.deleteFolder = function (req, res) {
+  let form = new formidable.IncomingForm()
+  form.parse(req, function (err, fields, file) {
+    if (err) throw err
+    let targetfloder = path.join(__dirname, '../upload', fields['flodername'])
+    fsExtra.remove(targetfloder, (err) => {
+      if (err) throw err
+      res.send('1')
+    })
+  })
+}
+module.exports.deleteImg = function (req, res) {
+  let form = new formidable.IncomingForm()
+  form.parse(req, function (err, fields, file) {
+    if (err) throw err
+    let img = path.basename(fields.imgSrc)
+    let target = path.join(__dirname, '../upload', fields['folderName'], img)
+    fsExtra.remove(target, (err) => {
+      if (err) throw err
+      res.send('1')
+    })
+  })
+}
